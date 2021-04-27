443e8eb21886f71ac5e473c50e57563f10a7b29f
MengLi2333 <731630226@qq.com>
2021-04-27 12:41:01 +0800

    init repository

---
+++ hdhr/src/main/java/cn/edu/cuit/hdhr/HdhrApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.cuit.hdhr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HdhrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HdhrApplication.class, args);
+    }
+
+}
